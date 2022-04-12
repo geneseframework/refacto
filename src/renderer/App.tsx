ab9aeae79c57ef350a01fa41185b5e8805499d69
gillesfabre34 <gillesfabre34@gmail.com>
2022-04-12 10:14:09 +0200

    Refacto main and app.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,24 +1,28 @@
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import { Dashboard } from '../pages/dashboard/Dashboard';
 import { CodeCoverage } from '../pages/code-coverage/CodeCoverage';
 import { CodeDuplication } from '../pages/code-duplication/CodeDuplication';
 import { Complexity } from '../pages/complexity/Complexity';
+import { JscpdReport } from '../features/jscpd/interfaces/JscpdReport.interface';
+// import { Settings } from '../pages/settings/Settings';
 
 declare global {
     interface Window {
         electron: {
             store: {
                 get: (key: string) => any;
-                jscpd: () => any;
+                getJscpdReport: () => JscpdReport | undefined;
                 setBrowserView: (route: string) => any;
                 removeBrowserViews: () => any;
-                run: (script: string) => any;
+                runJscpd: () => any;
                 set: (key: string, val: any) => void;
             };
         };
     }
 }
+
+export const store = window.electron.store;
 
 export default function App() {
     return (
@@ -28,6 +32,7 @@ export default function App() {
                 <Route path="/complexity" element={<Complexity />} />
                 <Route path="/code-coverage" element={<CodeCoverage />} />
                 <Route path="/code-duplication" element={<CodeDuplication />} />
+                {/*<Route path="/settings" element={<Settings />} />*/}
             </Routes>
         </Router>
     );
