ba518d06f31b8bbe3a8532bc19e1ef46c54ee3c3
gillesfabre34 <gillesfabre34@gmail.com>
2022-04-18 15:13:57 +0200

    Reset form

--- src/pages/settings/components/settingsRight/SettingsRight.hook.ts
+++ src/pages/settings/components/settingsRight/SettingsRight.hook.ts
@@ -1,22 +1,33 @@
 import { Project } from '../../../../shared/classes/project';
 import * as Yup from 'yup';
-import { FormikValues, useFormik } from 'formik';
+import { FormikState, FormikValues, useFormik } from 'formik';
+import { useState } from 'react';
 
 export const useSettingsRight = () => {
     const project: Project = window.electron.store.get('project');
-
+    const [initialValues] = useState({ name: project?.name, path: project?.path });
+    // const initialValues = { name: '', path: '' }
+    // const initialValues = new Project(project?.name, project?.path);
+    // const [initialValues, setInitialValues] = useState<Project>(new Project(project?.name, project?.path));
+    const [isNewProject, setIsNewProject] = useState<boolean>(!!project);
     const validationSchema = Yup.object({
         name: Yup.string().required('The name is required'),
         path: Yup.string().required('The path of the folder to analyze is required'),
     });
 
-    const initialValues = {
-        name: project?.name,
-        path: project?.path,
+    const cancel = () => {
+        console.log('CLEAR values')
+        formik.resetForm()
     }
 
     const onSubmit = (values: FormikValues) => {
         console.log('submit values', values)
+    }
+
+    const handleClickOnNewProject = () => {
+        // setIsNewProject(true);
+        formik.resetForm({ values: { name: 'zzz', path: 'fdsfds'}})
+        console.log('formik.values', formik.values)
     }
 
     const formik = useFormik({
@@ -28,5 +39,9 @@ export const useSettingsRight = () => {
 
     return {
         formik,
+        handleNewProject: handleClickOnNewProject,
+        initialValues,
+        isNewProject,
+        onClear: cancel,
     }
 }
