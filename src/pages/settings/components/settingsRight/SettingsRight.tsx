83d4f911587177fc164ca783230115d84938f060
gillesfabre34 <gillesfabre34@gmail.com>
2022-04-18 12:40:39 +0200

    Added Formik and Yup

--- src/pages/settings/components/settingsRight/SettingsRight.tsx
+++ src/pages/settings/components/settingsRight/SettingsRight.tsx
@@ -1,49 +1,50 @@
 import React from 'react';
 import './SettingsRight.scss';
-import { Box, Button, TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { useSettingsRight } from './SettingsRight.hook';
+
 
 export const SettingsRight: React.FC = () => {
     const h = useSettingsRight();
     console.log('h.project', h.name)
     return (
         <div className='rightContainer'>
-                <>
-                    <div className='title'>Project</div>
-                    <Box
-                        component="form"
-                        sx={{
-                            '& .MuiTextField-root': { m: 1, width: '25ch' },
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <div className="formRow">
-                            <TextField
-                                sx={{ flex: 4}}
-                                required
-                                id="name"
-                                label="Name"
-                                size="small"
-                                defaultValue={h.name}
-                            />
-                            <div className="flex7"/>
-                        </div>
-                        <div className="formRow">
-                            <TextField
-                                sx={{ flex: 10}}
-                                required
-                                id="name"
-                                label="Path"
-                                size="small"
-                                defaultValue={h.path}
-                            />
-                        </div>
-                        <div className="divSubmit">
-                            <Button variant="contained">Submit</Button>
-                        </div>
-                    </Box>
-                </>
+            <>
+                <div className='title'>Project</div>
+                    <form onSubmit={h.formik.handleSubmit}>
+                            <div className="formRow">
+                                <TextField
+                                    sx={{ flex: 4}}
+                                    id="name"
+                                    name="name"
+                                    label="Name"
+                                    size="small"
+                                    value={h.formik.values.name}
+                                    error={h.formik.touched.name && Boolean(h.formik.errors.name)}
+                                    helperText={h.formik.touched.name && h.formik.errors.name}
+                                    onChange={h.formik.handleChange('name')}
+                                    onBlur={h.formik.handleBlur('name')}
+                                />
+                                <div className="flex7"/>
+                            </div>
+                            <div className="formRow">
+                                <TextField
+                                    sx={{ flex: 10}}
+                                    name="path"
+                                    label="Path"
+                                    size="small"
+                                    value={h.formik.values.path}
+                                    error={h.formik.touched.path && Boolean(h.formik.errors.path)}
+                                    helperText={h.formik.touched.path && h.formik.errors.path}
+                                    onChange={h.formik.handleChange('path')}
+                                    onBlur={h.formik.handleBlur('path')}
+                                />
+                            </div>
+                            <div className="divSubmit">
+                                <Button disabled={!h.formik.isValid} variant="contained" type="submit">Submit</Button>
+                            </div>
+                    </form>
+            </>
         </div>
     );
 }
