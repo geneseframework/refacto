fb5aa30baed93ea4ef77bf972627150014f647f9
gillesfabre34 <gillesfabre34@gmail.com>
2022-04-24 13:39:16 +0200

    Adding refresh icon

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DashboardJscpd } from './jscpd/DashboardJscpd';
-import './Dashboard.scss'
+import './Dashboard.scss';
 import { NavBar } from '../../shared/components/NavBar/NavBar';
 import { useDashboard } from './Dashboard.hook';
 
@@ -8,20 +8,26 @@ export const Dashboard: React.FC = () => {
     const h = useDashboard();
 
     return (
-        <div className='bodyContainer'>
+        <div className="bodyContainer">
             <NavBar />
-            <div className='mainContainer dashboard'>
-                <div className='leftContainer'>
+            <div className="mainContainer dashboard">
+                <div className="leftContainer">
                     <p>{`Project : ${h.projectName}`}</p>
                     <h2>Cognitive complexity</h2>
                 </div>
-                <div className='rightContainer'>
+                <div className="rightContainer">
                     <div>
                         <h2>Code coverage</h2>
                     </div>
-                    <div><h2>Code smells</h2></div>
                     <div>
-                        {h.project && <DashboardJscpd duplicationStats={h.project?.stats?.duplication} />}
+                        <h2>Code smells</h2>
+                    </div>
+                    <div>
+                        {h.project && (
+                            <DashboardJscpd
+                                duplicationStats={h.project?.stats?.duplication}
+                            />
+                        )}
                     </div>
                 </div>
             </div>
